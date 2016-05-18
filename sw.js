bf67d274dc8c33c3c98a06e4ffd73c261f99bcaf
Mikhail Dunaev <dunaev.m@gmail.com>
2016-05-18 13:21:43 +0300

    upd

--- sw.js
+++ sw.js
@@ -9,6 +9,8 @@ this.addEventListener('fetch', function(event){
   console.log(event)
   event.respondWith(fetch(event.request).catch(function(err){
     console.log('offline')
-    return new Response('Offline mode!');
+    return new Response('<h1>Offline mode!</h1>', {
+      headers: { 'Content-Type': 'text/html' }
+    });
   }))
 })
