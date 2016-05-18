acfca658e3fd1dad217596a71babc2d49483bda8
Mikhail Dunaev <dunaev.m@gmail.com>
2016-05-18 14:41:11 +0300

    upd

--- sw.js
+++ sw.js
@@ -5,14 +5,9 @@ this.addEventListener('install', function(event){
 })
 
 this.addEventListener('fetch', function(event){
-  console.log('fetch')
-  console.log(event)
-  var response = new Response('<h1>Offline mode!</h1>')
-  // event.respondWith( response );
-
-  // event.respondWith( fetch(event.request.url).catch(function(err){
-  //     var response = new Response('<h1>Offline mode!</h1>')
-  //     return response
-  //   })
-  // )
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return new Response("Request failed!");
+    })
+  );
 })
