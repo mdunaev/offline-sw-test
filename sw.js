c2c391b8f0e8e5a10cb601b53b7b1293c34acb1c
Mikhail Dunaev <dunaev.m@gmail.com>
2016-05-18 12:45:48 +0300

    upd

--- sw.js
+++ sw.js
@@ -2,8 +2,9 @@ console.log('hallo from service worker!')
 
 this.addEventListener('install', function(event){
     console.log('installed!')
-});
+})
 
 this.addEventListener('fetch', function(event){
-  console.log(event);
-});
+  console.log('fetch')
+  console.log(event)
+})
